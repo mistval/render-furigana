63231c4c07d7f7f9fb0ce61d521474889f9ee34b
mistval <randallschmidt1@gmail.com>
2017-10-30 04:56:58 -0500

    rewrite, using spaces to separate kanji, and printing the kanji as whole lines, in order to avoid lines breaking in awkward places

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const reload = require('require-reload')(require);
 const Canvas = require('canvas');
 const fs = require('fs');
 const assert = require('assert');
+const HAIR_SPACE = '\u200A';
 
 let kuroshiro;
 try {
@@ -66,195 +67,213 @@ function extractFurigana(text) {
   });
 }
 
+class PositionedChunk {
+  constructor(x, chunk) {
+    this.x = x,
+    this.chunk = chunk;
+  }
+}
+
 class Line {
-  constructor(paddingBetweenFuriganaAndKanjiInPixels) {
-    this.chunks_ = [];
-    this.widthInPixels_ = 0;
-    this.heightInPixels_ = 0;
-    this.paddingBetweenFuriganaAndKanjiInPixels_ = paddingBetweenFuriganaAndKanjiInPixels;
-    this.furiganaPartHeightInPixels_ = 0;
-    this.kanjiPartHeightInPixels_ = 0;
+  constructor(maxWidth, paddingBetweenFuriganaAndKanji) {
+    this.paddingBetweenFuriganaAndKanji_ = paddingBetweenFuriganaAndKanji;
+    this.maxWidth_ = maxWidth;
+    this.widthRemaining_ = maxWidth;
+    this.height_ = 0;
+    this.positionedChunks = [];
+    this.furiganaPartHeight_ = 0;
+    this.kanjiPartHeight_ = 0;
+  }
+
+  canAddChunk(chunk) {
+    return chunk.calculateWidth() <= this.widthRemaining_;
   }
 
   addChunk(chunk) {
-    this.widthInPixels_ += Math.max(chunk.kanjiWidthInPixels || 0, chunk.furiganaWidthInPixels || 0);
-    this.furiganaPartHeightInPixels_ = Math.max(this.furiganaPartHeightInPixels_ || 0, chunk.furiganaHeightInPixels || 0);
-    this.kanjiPartHeightInPixels_ = Math.max(this.kanjiPartHeightInPixels_ || 0, chunk.kanjiHeightInPixels || 0);
-    this.chunks_.push(chunk);
+    let x = this.maxWidth_ - this.widthRemaining_;
+    this.widthRemaining_ -= chunk.calculateWidth();
+    this.positionedChunks.push(new PositionedChunk(x, chunk));
+    this.furiganaPartHeight_ = Math.max(this.furiganaPartHeight_, chunk.getFuriganaHeight());
+    this.kanjiPartHeight_ = Math.max(this.kanjiPartHeight_, chunk.getKanjiHeight());
   }
 
   getWidth() {
-    return this.widthInPixels_;
+    return this.maxWidth_ - this.widthRemaining_;
   }
 
-  getHeight() {
-    return this.furiganaPartHeightInPixels_ + this.kanjiPartHeightInPixels_ + this.paddingBetweenFuriganaAndKanjiInPixels_;
-  }
-
-  hasSpaceLeft(kanjiWidth, furiganaWidth, leftPaddingInPixels, rightPaddingInPixels, maxWidthInPixels) {
-    let chunkWidthInPixels = Math.max(kanjiWidth || 0, furiganaWidth || 0);
-    return chunkWidthInPixels <= maxWidthInPixels - this.getWidth() - (leftPaddingInPixels + rightPaddingInPixels);
-  }
-
-  getRenderInformation() {
-    let informations = [];
-    let nextChunkStartLeftInPixels = 0;
-    for (let chunk of this.chunks_) {
-      let chunkWidthInPixels = Math.max(chunk.kanjiWidthInPixels || 0, chunk.furiganaWidthInPixels || 0);
-      let furiganaStartTopInPixels = chunk.furiganaActualBoundingBoxAscentInPixels;
-      let kanjiStartTopInPixels = this.furiganaPartHeightInPixels_
-        + this.paddingBetweenFuriganaAndKanjiInPixels_
-        + chunk.kanjiActualBoundingBoxAscentInPixels
-        + (this.kanjiPartHeightInPixels_ - chunk.kanjiHeightInPixels) / 2;
-      let furiganaStartLeftInPixels = nextChunkStartLeftInPixels + (chunkWidthInPixels - chunk.furiganaWidthInPixels) / 2;
-      let kanjiStartLeftInPixels = nextChunkStartLeftInPixels + (chunkWidthInPixels - chunk.kanjiWidthInPixels) / 2;
-      informations.push({
-        kanji: chunk.kanji,
-        furigana: chunk.furigana,
-        furiganaStartTopInPixels: furiganaStartTopInPixels,
-        furiganaStartLeftInPixels: furiganaStartLeftInPixels,
-        kanjiStartTopInPixels: kanjiStartTopInPixels,
-        kanjiStartLeftInPixels: kanjiStartLeftInPixels,
-      });
-      nextChunkStartLeftInPixels += chunkWidthInPixels;
+  calculateHeight() {
+    let height = this.furiganaPartHeight_ + this.kanjiPartHeight_;
+    if (this.furiganaPartHeight_ !== 0) {
+      height += this.paddingBetweenFuriganaAndKanji_;
     }
+    return height;
+  }
 
-    return informations;
+  drawKanji(ctx, xOffset, yOffset) {
+    let kanjiString = '';
+    for (let positionedChunk of this.positionedChunks) {
+      kanjiString += positionedChunk.chunk.kanji;
+    }
+    let kanjiStringMetrics = ctx.measureText(kanjiString);
+    ctx.fillText(
+      kanjiString,
+      xOffset,
+      yOffset + kanjiStringMetrics.actualBoundingBoxAscent + this.furiganaPartHeight_ + this.paddingBetweenFuriganaAndKanji_);
+  }
+
+  drawFurigana(ctx, xOffset, yOffset) {
+    for (let positionedChunk of this.positionedChunks) {
+      ctx.fillText(
+        positionedChunk.chunk.furigana,
+        xOffset + positionedChunk.x + positionedChunk.chunk.furiganaXOffset,
+        yOffset + positionedChunk.chunk.getFuriganaYOffset());
+    }
   }
 }
 
-function getWidthAndHeightAndActualBoundingBoxAscent(ctx, text) {
-  let measurement = ctx.measureText(text);
-  let widthInPixels = measurement.width;
-  let heightInPixels = measurement.actualBoundingBoxAscent + measurement.actualBoundingBoxDescent;
-  return [widthInPixels, heightInPixels, measurement.actualBoundingBoxAscent];
-}
-
-function calculatePageWidth(lines) {
-  let widthInPixels = 0;
-  for (let line of lines) {
-    widthInPixels = Math.max(widthInPixels, line.getWidth());
+class Chunk {
+  constructor(kanji, furigana) {
+    this.kanji = kanji;
+    this.furigana = furigana || '';
+    this.furiganaXOffset = 0;
+    this.kanjiXOffset_ = 0;
   }
-  return widthInPixels;
-}
 
-function calculatePageHeight(lines, interLinePaddingInPixels) {
-  let heightInPixels = 0;
-  for (let line of lines) {
-    heightInPixels += line.getHeight();
+  calculateKanjiMetrics(ctx) {
+    this.kanjiMetrics_ = ctx.measureText(this.kanji);
   }
-  return heightInPixels + (lines.length - 1) * interLinePaddingInPixels;
+
+  calculateFuriganaMetrics(ctx) {
+    this.furiganaMetrics_ = ctx.measureText(this.furigana);
+  }
+
+  calculateWidth() {
+    return Math.max(this.kanjiMetrics_.width, this.furiganaMetrics_.width);
+  }
+
+  getHeight_(metrics) {
+    return metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+  }
+
+  getFuriganaHeight() {
+    return this.getHeight_(this.furiganaMetrics_);
+  }
+
+  getKanjiHeight() {
+    return this.getHeight_(this.kanjiMetrics_);
+  }
+
+  getFuriganaYOffset() {
+    return this.furiganaMetrics_.actualBoundingBoxAscent;
+  }
+
+  addPaddingAndCalculateOffsets(hairSpaceWidth) {
+    assert(this.kanjiMetrics_ && this.furiganaMetrics_, 'Need to call calculateKanjiMetrics and calculateFuriganaMetrics first');
+    this.kanjiXOffset_ = Math.max((this.furiganaMetrics_.width - this.kanjiMetrics_.width) / 2, 0);
+    this.furiganaXOffset = Math.max((this.kanjiMetrics_.width - this.furiganaMetrics_.width) / 2, 0);
+    if (this.kanji.indexOf('上') !== -1) {
+      debugger;
+    }
+    if (this.kanjiXOffset_ > 0) {
+      let spacePaddingNeededPerSide = Math.ceil(this.kanjiXOffset_ / hairSpaceWidth);
+      let spacePaddingPerSide = Array(spacePaddingNeededPerSide + 1).join(HAIR_SPACE);
+      this.kanji = spacePaddingPerSide + this.kanji + spacePaddingPerSide;
+      this.kanjiMetrics_.width += spacePaddingNeededPerSide * 2 * hairSpaceWidth;
+    }
+  }
 }
 
-function draw(chunks, kanjiFont, furiganaFont, options) {
+function draw(rawChunks, kanjiFont, furiganaFont, options) {
   assert(typeof kanjiFont === typeof '', 'No kanji font provided. You must provide a font name as a string.');
   assert(typeof furiganaFont === typeof '', 'No furigana font provided. You must provide a font name as a string.');
   options = options || {};
-  let maxAllowedWidthInPixels = options.maxWidthInPixels || 1000;
-  let minAllowedWidthInPixels = options.minWidthInPixels || 0;
-  let maxAllowedHeightInPixels = options.maxHeightInPixels || Number.MAX_SAFE_INTEGER;
-  let minAllowedHeightInPixels = options.minHeightInPixels || 0;
-  let leftPaddingInPixels = options.leftPaddingInPixels || 0;
-  let rightPaddingInPixels = options.rightPaddingInPixels || 0;
-  let topPaddingInPixels = options.topPaddingInPixels || 0;
-  let bottomPaddingInPixels = options.bottomPaddingInPixels || 0;
-  let paddingBetweenFuriganaAndKanjiInPixels = options.paddingBetweenFuriganaAndKanjiInPixels || 3;
-  let paddingBetweenLinesInPixels = options.paddingBetweenLinesInPixels || 5;
+  let maxAllowedWidth = options.maxWidthInPixels || 1000;
+  let minAllowedWidth = options.minWidthInPixels || 0;
+  let maxAllowedHeight = options.maxHeightInPixels || Number.MAX_SAFE_INTEGER;
+  let minAllowedHeight = options.minHeightInPixels || 0;
+  let leftPadding = options.leftPaddingInPixels || 100;
+  let rightPadding = options.rightPaddingInPixels || 10;
+  let topPadding = options.topPaddingInPixels || 10;
+  let bottomPadding = options.bottomPaddingInPixels || 10;
+  let paddingBetweenFuriganaAndKanji = options.paddingBetweenFuriganaAndKanjiInPixels || 3;
+  let paddingBetweenLines = options.paddingBetweenLinesInPixels || 10;
   let backgroundColor = options.backgroundColor || 'white';
   let textColor = options.backgroundColor || 'black';
+
+  let chunks = [];
+  for (let rawChunk of rawChunks) {
+    chunks.push(new Chunk(rawChunk.kanji, rawChunk.furigana));
+  }
 
   let canvas = new Canvas(0, 0);
   let ctx = canvas.getContext('2d');
 
-  // Calculate the width of each chunk's Kanji
+  // Calculate the kanji metrics
   ctx.font = kanjiFont;
+  let hairSpaceWidth = ctx.measureText(HAIR_SPACE).width;
   for (let chunk of chunks) {
-    let kanji = chunk.kanji;
-    if (kanji) {
-      [chunk.kanjiWidthInPixels, chunk.kanjiHeightInPixels, chunk.kanjiActualBoundingBoxAscentInPixels] =
-        getWidthAndHeightAndActualBoundingBoxAscent(ctx, kanji);
-    }
+    chunk.calculateKanjiMetrics(ctx);
   }
 
-  // Calculate the width of each chunk's Furigana
-  // (We calculate the furigana and kanji width in separate loops because setting the font on the canvas context is pretty slow)
+  // Calculate the furigana metrics
+  // (We calculate the furigana and kanji netrics in separate loops because setting the font on the canvas context is pretty slow)
   ctx.font = furiganaFont;
   for (let chunk of chunks) {
-    let furigana = chunk.furigana;
-    if (furigana) {
-      [chunk.furiganaWidthInPixels, chunk.furiganaHeightInPixels, chunk.furiganaActualBoundingBoxAscentInPixels] =
-        getWidthAndHeightAndActualBoundingBoxAscent(ctx, furigana);
-    }
+    chunk.calculateFuriganaMetrics(ctx);
   }
 
-  // Organize the results into lines
+  for (let chunk of chunks) {
+    chunk.addPaddingAndCalculateOffsets(hairSpaceWidth);
+  }
+
   let lines = [];
-  let currentLine = new Line(paddingBetweenFuriganaAndKanjiInPixels);
+  let currentLine = new Line(maxAllowedWidth, paddingBetweenFuriganaAndKanji);
   lines.push(currentLine);
   for (let chunk of chunks) {
-    let lineHasSpaceLeft = currentLine.hasSpaceLeft(
-      chunk.kanjiWidthInPixels,
-      chunk.furiganaWidthInPixels,
-      leftPaddingInPixels,
-      rightPaddingInPixels,
-      maxAllowedWidthInPixels);
-
-    if (!lineHasSpaceLeft) {
-      currentLine = new Line(paddingBetweenFuriganaAndKanjiInPixels);
+    if (!currentLine.canAddChunk(chunk)) {
+      currentLine = new Line(maxAllowedWidth, paddingBetweenFuriganaAndKanji);
       lines.push(currentLine);
     }
-
     currentLine.addChunk(chunk);
   }
 
+  let totalHeight = 0;
+  for (let line of lines) {
+    totalHeight += line.calculateHeight();
+    totalHeight += paddingBetweenLines;
+  }
+  totalHeight -= paddingBetweenLines;
+  totalHeight += topPadding;
+  totalHeight += bottomPadding;
+
+  let maxWidth = 0;
+  for (let line of lines) {
+    maxWidth = Math.max(maxWidth, line.getWidth());
+  }
+  maxWidth += leftPadding;
+  maxWidth += rightPadding;
+
   // Set the canvas to the correct size.
-  canvas.width = Math.max(
-    Math.min(
-      calculatePageWidth(lines) + leftPaddingInPixels + rightPaddingInPixels,
-      maxAllowedWidthInPixels),
-    minAllowedWidthInPixels);
-  canvas.height = Math.max(
-    Math.min(
-      calculatePageHeight(lines, paddingBetweenLinesInPixels) + leftPaddingInPixels + rightPaddingInPixels,
-      maxAllowedHeightInPixels),
-    minAllowedHeightInPixels);
+  canvas.width = maxWidth;
+  canvas.height = totalHeight;
 
   ctx.fillStyle = backgroundColor;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  // Draw the results on the canvas.
-  // One loop for kanji and one loop for furigana.
-  // Because we save a lot of time if we only set the font twice.
   ctx.fillStyle = textColor;
-  let lineStartLeftInPixels = leftPaddingInPixels;
-  let lineStartTopInPixels = topPaddingInPixels;
-  ctx.font = furiganaFont;
+  // Draw the results. Furigana first
+  let xOffset = leftPadding;
+  let yOffset = topPadding;
   for (let line of lines) {
-    let renderInformations = line.getRenderInformation();
-    for (let renderInformation of renderInformations) {
-      if (renderInformation.furigana) {
-        ctx.fillText(
-          renderInformation.furigana,
-          lineStartLeftInPixels + renderInformation.furiganaStartLeftInPixels,
-          lineStartTopInPixels + renderInformation.furiganaStartTopInPixels);
-      }
-    }
-    lineStartTopInPixels += line.getHeight() + paddingBetweenLinesInPixels;
+    line.drawFurigana(ctx, xOffset, yOffset);
+    yOffset += line.calculateHeight() + paddingBetweenLines;
   }
-  lineStartLeftInPixels = leftPaddingInPixels
-  lineStartTopInPixels = topPaddingInPixels
+
   ctx.font = kanjiFont;
+  yOffset = topPadding;
   for (let line of lines) {
-    let renderInformations = line.getRenderInformation();
-    for (let renderInformation of renderInformations) {
-      if (renderInformation.kanji) {
-        ctx.fillText(
-          renderInformation.kanji,
-          lineStartLeftInPixels + renderInformation.kanjiStartLeftInPixels,
-          lineStartTopInPixels + renderInformation.kanjiStartTopInPixels);
-      }
-    }
-    lineStartTopInPixels += line.getHeight() + paddingBetweenLinesInPixels;
+    line.drawKanji(ctx, xOffset, yOffset);
+    yOffset += line.calculateHeight() + paddingBetweenLines;
   }
 
   return new Promise((fulfill, reject) => {
@@ -274,7 +293,7 @@ function draw(chunks, kanjiFont, furiganaFont, options) {
 * In the case of automatically detected furigana, it leads to more natural line breaking.
 */
 function splitNonFuriganaChunks(chunks) {
-  let newChunks = [];
+  let newRawChunks = [];
   let nextNonKanaChunk;
   for (let chunk of chunks) {
     if (!chunk.furigana) {
@@ -285,29 +304,28 @@ function splitNonFuriganaChunks(chunks) {
           if (nextNonKanaChunk) {
             nextNonKanaChunk = undefined;
           }
-          newChunks.push({kanji: character});
+          newRawChunks.push({kanji: character});
         } else {
           if (!nextNonKanaChunk) {
             nextNonKanaChunk = {kanji: ''};
-            newChunks.push(nextNonKanaChunk);
+            newRawChunks.push(nextNonKanaChunk);
           }
           nextNonKanaChunk.kanji += character;
         }
       }
     } else {
-      newChunks.push(chunk);
+      newRawChunks.push(chunk);
     }
   }
-
-  return newChunks;
+  return newRawChunks;
 }
 
 module.exports = function(text, kanjiFont, furiganaFont, options) {
   assert(text, 'No text provided. You must provide a string or an array of {kanji:xx, furigana:xx}');
   if (typeof text === typeof '') {
-    return extractFurigana(text).then(chunks => {
-      chunks = splitNonFuriganaChunks(chunks);
-      return draw(chunks, kanjiFont, furiganaFont, options);
+    return extractFurigana(text).then(rawChunks => {
+      rawChunks = splitNonFuriganaChunks(rawChunks);
+      return draw(rawChunks, kanjiFont, furiganaFont, options);
     });
   } else {
     return draw(text, kanjiFont, furiganaFont, options);
